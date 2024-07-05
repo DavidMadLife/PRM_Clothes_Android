5bc81da4f89cf0578e92e2ad3984d5016539a35e
dgphus <dphus1507@gmail.com>
2024-07-05 20:29:43 +0700

    config firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,14 @@ plugins {
     alias(libs.plugins.google.gms.google.services) apply false
     //id("com.google.gms.google-services") version ("4.4.2") apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.4.0")
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
